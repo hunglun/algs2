2549e126b0b6b7960683a80f614cf7d350c05e5e
Hung Lun <hunglun.o@gmail.com>
2017-12-18 18:33:25 +0800

    created stub files for word net assignment

---
+++ wordnet/Outcast.java
@@ -0,0 +1,10 @@
+public class Outcast {
+    public Outcast(WordNet wordnet){}         // constructor takes a WordNet object
+    public String outcast(String[] nouns){
+	return "";
+    }   // given an array of WordNet nouns, return an outcast
+   public static void main(String[] args)  // see test client below
+    {
+
+    }
+}
