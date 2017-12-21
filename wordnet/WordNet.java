e9a001c84326f2745651df98620bade1e9465fd5
Hung Lun <hunglun.o@gmail.com>
2017-12-21 17:44:10 +0800

    use digraph to store hypernyms data.

--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
@@ -13,9 +14,10 @@ public class WordNet {
     synsetsTree = new TreeSet<Synset>();
     String line;
     String fields[];
-    
+    ST<Integer, Synset> st = new ST<Integer, Synset>();
     in = new In(synsets);
     line = in.readLine();
+    int table_id = 0;
   
     int synset_id;    
     while(line != null){
@@ -26,7 +28,8 @@ public class WordNet {
       synset_id = Integer.parseInt(fields[0]);
       StdOut.printf("id = %d, nouns = %s, definition = %s\n", synset_id, fields[1], fields[2]);             
       line = in.readLine();
-      synsetsTree.add(new Synset(synset_id,fields[1].split(" "),fields[2]));  
+      synsetsTree.add(new Synset(synset_id,fields[1].split(" "),fields[2]));
+      st.put(synset_id, new Synset(table_id++,fields[1].split(" "),fields[2]));
        // create a BST from synsets.txt, making isNoun return in logarithmic time
     } 
     
@@ -44,6 +47,9 @@ public class WordNet {
       StdOut.printf("synsetid = %d, hypernyms: ", Integer.parseInt(fields[0]));
       for(int i = 1; i < fields.length; i++){
         StdOut.printf("%d ", Integer.parseInt(fields[i]));
+        int v = st.get(Integer.parseInt(fields[0])).id;
+        int w = st.get(Integer.parseInt(fields[i])).id;
+        digraph.addEdge(v,w);
       }
       StdOut.printf("\n");                
       line = in.readLine();
@@ -51,16 +57,16 @@ public class WordNet {
     } 
   }
   private class Synset implements Comparable<Synset>{
-    private int id;
+    public final int id;
     public final String nouns[];
     private String definition;
+
     public Synset(int id, String nouns[], String definition){
       this.id = id;
       this.nouns = nouns;
       this.definition = definition;
     }
     public int compareTo(Synset that) {
-      
       for(String thisNoun : nouns){
         for(String thatNoun : that.nouns){
           if (thisNoun.compareTo(thatNoun) == 0) return 0;
