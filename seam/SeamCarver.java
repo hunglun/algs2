0e003d91324842d94c86cfc6a79a1800d25f277d
Hung Lun <hunglun.o@gmail.com>
2017-12-28 16:48:13 +0800

    added corner cases to energy and remove*seam methods.

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -13,6 +13,7 @@ public class SeamCarver {
   private Color[][] p_color;
   public SeamCarver(Picture picture)                {
     // compute energy matrix
+    if (picture == null)  throw new IllegalArgumentException("error");
     p = picture;
     w = p.width();
     h = p.height();
@@ -82,7 +83,10 @@ public class SeamCarver {
     
     return r*r + g*g + b*b;
   }
-  public  double energy(int x, int y)               {
+  public  double energy(int x, int y)       
+ {
+    if (x>w-1 || y >h-1 || x < 0 || y <0)
+      throw new IllegalArgumentException("error");
     if (x == 0 || y == 0 || y == this.h - 1 || x == this.w -1) return 1000;
     return Math.sqrt(gx(x,y) + gy(x,y));
   } // energy of pixel at column x and row y
@@ -103,9 +107,17 @@ public class SeamCarver {
 
   } // sequence of indices for vertical seam
   public    void removeHorizontalSeam(int[] seam)   {
+    int prev = seam[0];
+    for(int i : seam){
+      if (Math.abs(i - prev) > 1) throw new IllegalArgumentException("error");
+      if(i < 0 ||  i > h -1)  throw new IllegalArgumentException("error");
+      prev = i;
+    }
+    
     h = h - 1;
     for(int i = 0; i < w ; i++){
      for(int j = 0; j < h; j++){
+       
        if (j >= seam[i]){
          p_color[i][j] = p_color[i][j+1];
        }
@@ -124,9 +136,17 @@ public class SeamCarver {
    sp = new SP(w,h,m);
   } // remove horizontal seam from current picture
   public    void removeVerticalSeam(int[] seam)     {
-   w = w - 1;
+    int prev = seam[0];
+   for(int i : seam){
+     if (Math.abs(i - prev) > 1) throw new IllegalArgumentException("error");
+     if(i < 0 ||  i > w -1)  throw new IllegalArgumentException("error");
+     prev = i;
+   }
+    
+    w = w - 1;
    for(int j = 0; j < h; j++){
      for(int i = 0; i < w ; i++){
+       
        if (i >= seam[j]){
          p_color[i][j] = p_color[i+1][j];
        }
