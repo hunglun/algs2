183be4b375a1fbf85d489342ab270c21e5f0cbe5
Hung Lun <hunglun.o@gmail.com>
2017-12-30 04:56:58 +0800

    added edges for game and team

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -3,9 +3,15 @@ import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FordFulkerson;
 
 public class BaseballElimination{
   private ST<String, Stat> scoreboard;
+  private int numberOfTeams;
   public BaseballElimination(String filename){
     if (filename == null) throw new IllegalArgumentException("invalid filename"); 
     In in = new In(filename);
@@ -16,14 +22,14 @@ public class BaseballElimination{
     scoreboard = new ST<String, Stat>();
     
     // reading the input
-    int num = in.readInt();
-    game = new int[num];
-    for(int i=0; i<num; i++){
+    numberOfTeams = in.readInt();
+    game = new int[numberOfTeams];
+    for(int i=0; i<numberOfTeams; i++){
       name = in.readString();
       win = in.readInt();
       loss = in.readInt();
       remain = in.readInt();
-      for(int j=0; j<num; j++){
+      for(int j=0; j<numberOfTeams; j++){
         game[j]=in.readInt();
       }
       scoreboard.put(name, new Stat(i, win,loss,remain,game));
@@ -73,34 +79,70 @@ public class BaseballElimination{
   }    // number of remaining games between team1 and team2
   public          boolean isEliminated(String team){
     // trivial case
-    if (isTriviallyEliminated(team)) return true;
+    if (trivialCase(team).size() > 0 ) return true;
     
-    // nontrivial case
+    // nontriviarl case
+    // use arithmetic sum to compute the total number of binary combination of remaing teams.
+    int games = ((numberOfTeams - 1) * numberOfTeams) / 2;
+   
+    FlowNetwork fn = new FlowNetwork(2 + games + (numberOfTeams - 1));
+    
+// add flow edges
+    
+    
+    Queue<Stat> otherTeams = new Queue<Stat>();
+    for(String key : scoreboard){
+      if(key == team) continue;
+      otherTeams.enqueue(scoreboard.get(key));
+    }
+    
+    // link game vertices and team vertices
+    int gameid = numberOfTeams;
+    Stat home;
+    while(!otherTeams.isEmpty()){
+      home = otherTeams.dequeue();
+      for (Stat guest : otherTeams){        
+        fn.addEdge(new FlowEdge(gameid, guest.id,Double.POSITIVE_INFINITY));
+        fn.addEdge(new FlowEdge(gameid, home.id,Double.POSITIVE_INFINITY));
+        gameid++;
+      }
+    }
+    
+    // link start with game vertices
+    /*int startid = gameid;
+    for(int i=numberOfTeams; i<startid; i++){
+      fn.addEdge(new FlowEdge(startid, i,Double.POSITIVE_INFINITY));
+    */
+    // link sink with team vertices
+    StdOut.println("Flow network:" + fn + " End"); 
     return true;
   }              // is given team eliminated?
   
-  private boolean isTriviallyEliminated(String team){
+
+  public Iterable<String> certificateOfElimination(String team){
+      Bag<String> result;
+      result = trivialCase(team);
+      if (result.size() > 0) return result;
+      
+      result = new Bag<String>();
+      
+      // non trivial case
+      return result;
+  }  // subset R of teams that eliminates given team; null if not eliminated
+  
+  // Private methods //////////////////////////////////////////////////////////
+  private Bag<String> trivialCase(String team){
     //w[x] + r[x] < w[i]
+    Bag<String> teams = new Bag<String>();
     int wx = wins(team);
     int rx = remaining(team);
     
     for(String other : teams()){
-      if (wx + rx < wins(other)) return true;
-    }
-    return false;
-  }
-  private Iterable<String> certificateOfTrivialElimination(String team){
-    
-    Bag<String> teams = new Bag<String>();
-    for(String t : teams()){
-      if (t !=  team) teams.add(t);        
+      if (wx + rx < wins(other)) teams.add(other);
     }
     return teams;
   }
-  public Iterable<String> certificateOfElimination(String team){
-    if (isTriviallyEliminated(team)) return certificateOfTrivialElimination(team);
-    return scoreboard.keys();
-  }  // subset R of teams that eliminates given team; null if not eliminated
+    
   public static void main(String[] args) {
     BaseballElimination division = new BaseballElimination(args[0]);
     for (String team : division.teams()) {
