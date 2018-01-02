d4041fd74f75874e8c35ee93c33bfa883f5829d9
Hung Lun <hunglun.o@gmail.com>
2018-01-02 21:10:54 +0800

    use Tree to represent dictionary

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -1,18 +1,23 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-
+import edu.princeton.cs.algs4.Stack;
+import java.util.TreeSet;
 
 public class BoggleSolver
 {
+  private TreeSet<String> dictset;
   // Initializes the data structure using the given array of strings as the dictionary.
   // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
   public BoggleSolver(String[] dictionary){
-    
+    dictset = new TreeSet<String>();
+    for(String word : dictionary){
+      dictset.add(word);
+    }
   }
   
   // Returns the set of all valid words in the given Boggle board, as an Iterable.
   public Iterable<String> getAllValidWords(BoggleBoard board){
-    return null;
+    return new Stack<String>();
   }
   
   // Returns the score of the given word if it is in the dictionary, zero otherwise.
