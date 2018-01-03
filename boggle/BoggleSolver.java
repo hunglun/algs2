0b1d77be997e183c2f36fbe4e052e9cec9caaa51
Hung Lun <hunglun.o@gmail.com>
2018-01-03 08:53:34 +0800

    added adj and clear_mark methods.

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -1,6 +1,6 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.Bag;
 import java.util.TreeSet;
 
 public class BoggleSolver
@@ -13,7 +13,7 @@ public class BoggleSolver
   // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
   public BoggleSolver(String[] dictionary){
     dictset = new TreeSet<String>();
-    word = new StringBuilder();
+    
     for(String word : dictionary){
       dictset.add(word);
     }
@@ -26,28 +26,55 @@ public class BoggleSolver
     }
   }
   
-  private Iterable<Pair> adj(Pair pair){
-    return new Stack<Pair>();
+  private Iterable<Pair> adj(BoggleBoard board, Pair pair){
+    Bag<Pair> bag = new Bag<Pair>();
+    //StdOut.printf("Adjacent cells to %d, %d\n",pair.row,pair.col);
+    for(int i=pair.col-1; i<pair.col+2; i++){
+      for(int j=pair.row-1; j<pair.row + 2; j++){
+        if( i<0 || j<0 || i>board.cols()-1 || j>board.rows()-1 || (i==pair.col && j==pair.row)) {
+         // StdOut.printf("Out of bound:%d, %d\n",j,i);        
+        }else{
+          //StdOut.printf("%d, %d\n",j,i);
+          bag.add(new Pair(i,j));
+        }
+      }
+    }
+      
+    return bag;
   }
-  
+  private void clear_mark(){
+    for(int i=0; i< marked.length; i++){
+      for(int j=0; j< marked[0].length; j++){
+        marked[i][j] = false;
+      }
+    }
+  }
   private void dfs(BoggleBoard board, int col, int row){
    marked[col][row] = true;
-   count++;
-   word.append(board.getLetter(row,col)); // String Builder TODO
-   for(Pair pair : adj(new Pair(col,row))){
-     if(!marked[pair.col][pair.row]) dfs(board,pair.col,pair.row);
+  // count++;
+   //word.append(board.getLetter(row,col)); 
+   for(Pair pair : adj(board, new Pair(col,row))){
+     if(!marked[pair.col][pair.row]) {
+       dfs(board,pair.col,pair.row);
+     }
    }
+  
     
+   
+   //word = new StringBuilder();
   }
+  
   // Returns the set of all valid words in the given Boggle board, as an Iterable.
   public Iterable<String> getAllValidWords(BoggleBoard board){
     marked = new boolean[board.cols()][board.rows()];
+    Bag<String> bag = new Bag<String>();
     for(int c = 0; c < board.cols(); c++){
       for(int r = 0; r < board.rows(); r++){
         dfs(board,c,r);
+        clear_mark();
       }
     }
-    return new Stack<String>();
+    return bag;
   }
   
   // Returns the score of the given word if it is in the dictionary, zero otherwise.
@@ -62,9 +89,11 @@ public class BoggleSolver
     String[] dictionary = in.readAllStrings();
     BoggleSolver solver = new BoggleSolver(dictionary);
     BoggleBoard board = new BoggleBoard(args[1]);
+    StdOut.println(board);
     int score = 0;
+    int _count = 0;
     for (String word : solver.getAllValidWords(board)) {
-      StdOut.println(word);
+      StdOut.println(_count++ + ":"+word);
       score += solver.scoreOf(word);
     }
     StdOut.println("Score = " + score);
