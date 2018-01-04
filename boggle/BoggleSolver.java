6a87ca33e3e885033a67c661e13df375a1fe9a2d
Hung Lun <hunglun.o@gmail.com>
2018-01-05 01:20:08 +0800

    enumerate all simple strings
    Create the data type BoggleSolver. Write a method based on
       depth-first search to enumerate all strings that can be composed by
       following sequences of adjacent dice. That is, enumerate all simple
       paths in the Boggle graph (but there is no need to explicitly form
       the graph). For now, ignore the special two-letter sequence Qu.

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -6,15 +6,16 @@ import java.util.TreeSet;
 public class BoggleSolver
 {
   private TreeSet<String> dictset;
+  private StringBuilder word;
   private boolean marked[][];
   private int count;
-  private StringBuilder word;
   private Bag<String> allValidWords;
   // Initializes the data structure using the given array of strings as the dictionary.
   // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
   public BoggleSolver(String[] dictionary){
     dictset = new TreeSet<String>();
     allValidWords = new Bag<String>();
+    count = 0;
     for(String word : dictionary){
       dictset.add(word);
     }
@@ -22,12 +23,12 @@ public class BoggleSolver
   private class Pair{
     public int col,row;
     public Pair(int i, int j){
-      col = i;
-      row = j;
+      row = i;
+      col = j;
     }
   }
   
-  private Iterable<Pair> adj(BoggleBoard board, Pair pair){
+  private Bag<Pair> adj(BoggleBoard board, Pair pair){
     Bag<Pair> bag = new Bag<Pair>();
     //StdOut.printf("Adjacent cells to %d, %d\n",pair.row,pair.col);
     for(int i=pair.col-1; i<pair.col+2; i++){
@@ -36,63 +37,48 @@ public class BoggleSolver
          // StdOut.printf("Out of bound:%d, %d\n",j,i);        
         }else{
           //StdOut.printf("%d, %d\n",j,i);
-          bag.add(new Pair(i,j));
+          if(!marked[j][i]) {
+            bag.add(new Pair(j,i));
+          }
         }
       }
     }
       
     return bag;
   }
-  private void clear_mark(){
-    for(int i=0; i< marked.length; i++){
-      for(int j=0; j< marked[0].length; j++){
-        marked[i][j] = false;
+
+  
+
+  
+  
+  private void dfs(BoggleBoard board, int row, int col){
+    
+    Bag<Pair> nbs = adj(board, new Pair(row,col));
+    
+    if(nbs.isEmpty()) allValidWords.add(word.toString());
+    else{
+      for(Pair pair : nbs){      
+        word.append(board.getLetter(pair.row,pair.col));
+        marked[pair.row][pair.col] = true;
+        dfs(board,pair.row,pair.col);
+        word.deleteCharAt(word.length()-1);
+        marked[pair.row][pair.col] = false;  
       }
     }
-  }
-  private void dfs(BoggleBoard board, int col, int row){
-   marked[col][row] = true;
-   //StdOut.printf("%c ", board.getLetter(row,col));
-   int orig_length = word.length();
-   word.append(board.getLetter(row,col)); 
-   
-   if (word.length() == 3) {
-      allValidWords.add(word.toString());
-      word.deleteCharAt(word.length()-1); //TODO cache word.size()
-    //  word.deleteCharAt(word.length()-1); //TODO cache word.size()
-      //word = new StringBuilder();
-      marked[col][row] = false;
-      return;
-   }
-   
-   
-   for(Pair pair : adj(board, new Pair(col,row))){
-     if(!marked[pair.col][pair.row]) {
-       dfs(board,pair.col,pair.row);
-     }
-   }
-  // word.delete(orig_length, word.length()-1);
-   //marked[col][row] = false;
-  
     
-   
-   //word = new StringBuilder();
   }
   
   // Returns the set of all valid words in the given Boggle board, as an Iterable.
   public Iterable<String> getAllValidWords(BoggleBoard board){
-    marked = new boolean[board.cols()][board.rows()];
+    
+    marked = new boolean[board.rows()][board.cols()];
     word = new StringBuilder();
+    marked[0][0]=true;
+    word.append(board.getLetter(0,0));
     dfs(board,0,0);
-    /*
-     * 
-    for(int c = 0; c < board.cols(); c++){
-      for(int r = 0; r < board.rows(); r++){
-        dfs(board,c,r);
-        clear_mark();
-      }
-    }
-    */
+    marked[0][0]=false;
+    word.deleteCharAt(word.length()-1);
+
     return allValidWords;
   }
   
@@ -111,10 +97,13 @@ public class BoggleSolver
     StdOut.println(board);
     int score = 0;
     int _count = 0;
+    
     for (String word : solver.getAllValidWords(board)) {
-      StdOut.println(_count++ + ":"+word);
+    //  StdOut.println(_count++ + ":"+word);
+      _count++;
       score += solver.scoreOf(word);
     }
+    StdOut.println("Count = " + _count);
     StdOut.println("Score = " + score);
   }
   
