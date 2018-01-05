9b7e17a92feae40cb6f72b298351bf91986f37db
Hung Lun <hunglun.o@gmail.com>
2018-01-06 00:25:07 +0800

    use a virtual start node to simplify code.
    add string length as a new validity condition.

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -31,6 +31,15 @@ public class BoggleSolver
   private Bag<Pair> adj(BoggleBoard board, Pair pair){
     Bag<Pair> bag = new Bag<Pair>();
     //StdOut.printf("Adjacent cells to %d, %d\n",pair.row,pair.col);
+    // if it is the virtual start node
+    if ((pair.row == -1) && (pair.col == -1)){
+      for(int j=0;j<board.cols(); j++){
+        for(int i=0;i<board.rows(); i++){
+        bag.add(new Pair(i,j));
+        }
+      }
+      return bag;
+    }
     for(int i=pair.col-1; i<pair.col+2; i++){
       for(int j=pair.row-1; j<pair.row + 2; j++){
         if( i<0 || j<0 || i>board.cols()-1 || j>board.rows()-1 || (i==pair.col && j==pair.row)) {
@@ -49,13 +58,15 @@ public class BoggleSolver
 
   
 
-  
+  private boolean isValid(String w){
+    return w.length()>2 && dictset.contains(w);
+  }
   
   private void dfs(BoggleBoard board, int row, int col){
     
     Bag<Pair> nbs = adj(board, new Pair(row,col));
     String wordstring = word.toString();
-    if(dictset.contains(wordstring)) allValidWords.add(wordstring);
+    if(isValid(wordstring)) allValidWords.add(wordstring);
     
     for(Pair pair : nbs){      
       word.append(board.getLetter(pair.row,pair.col));
@@ -73,12 +84,9 @@ public class BoggleSolver
     
     marked = new boolean[board.rows()][board.cols()];
     word = new StringBuilder();
-    marked[0][0]=true;
-    word.append(board.getLetter(0,0));
-    dfs(board,0,0);
-    marked[0][0]=false;
-    word.deleteCharAt(word.length()-1);
-
+    
+    dfs(board,-1,-1);
+    
     return allValidWords;
   }
   
@@ -99,7 +107,7 @@ public class BoggleSolver
     int _count = 0;
     
     for (String word : solver.getAllValidWords(board)) {
-      StdOut.println(_count++ + ":"+word);
+      StdOut.println(_count + ":"+word);
       _count++;
       score += solver.scoreOf(word);
     }
