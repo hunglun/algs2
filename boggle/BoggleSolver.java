0f7826ca34b90a493b66d0098226efb35db50346
Hung Lun <hunglun.o@gmail.com>
2018-01-06 01:33:31 +0800

    handle special case Qu

--- boggle/BoggleSolver.java
+++ boggle/BoggleSolver.java
@@ -20,6 +20,7 @@ public class BoggleSolver
     count = 0;
     st = new TrieST<Integer>();
     for(String word : dictionary){
+      word = word.replaceAll("QU","Q");
       dictset.add(word);
       st.put(word,count++);
     }
@@ -107,12 +108,14 @@ public class BoggleSolver
   // Returns the score of the given word if it is in the dictionary, zero otherwise.
   // (You can assume the word contains only the uppercase letters A through Z.)
   public int scoreOf(String word){
-      if (word.length() == 3) return 1;
-      if (word.length() == 4) return 1;
-      if (word.length() == 5) return 2;
-      if (word.length() == 6) return 3;
-      if (word.length() == 7) return 5;
-      if (word.length() >= 8) return 11;
+    int length = word.length();
+    length += word.split("Q").length - 1;
+      if (length == 3) return 1;
+      if (length == 4) return 1;
+      if (length == 5) return 2;
+      if (length == 6) return 3;
+      if (length == 7) return 5;
+      if (length >= 8) return 11;
       assert(false); // shouldn't be here.
       return 0;
   }
